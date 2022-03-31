61b2b471e38c28c94fd75511a558c20379f5e8ce
Ivan Baluta <ltlaitoff@gmail.com>
2022-03-31 20:07:34 +0300

    Added use RecornInfo and PointsButton

--- src/components/Post/Post.tsx
+++ src/components/Post/Post.tsx
@@ -1,77 +1,42 @@
 import React, { FC, useState } from 'react'
-import { Link } from 'react-router-dom'
 import classNames from 'classnames'
 
-// @ts-ignore
-import { PostProps } from './Post.interfaces.tsx'
-import PostButtons from './components/PostButtons'
+import { PostProps } from './Post.interfaces'
 
-import { timeSince } from 'utils'
+import RecordInfo from 'components/RecordInfo'
+import PointsButton from '../PointsButton/PointsButton'
 
 const Post: FC<PostProps> = ({ data, className }: PostProps) => {
 	const [vote, setVote] = useState<boolean>(false)
 
 	const toogleVote = () => setVote(currentVote => !currentVote)
 
-	const commentsLink = '/comments'
-	const profileLink = '/profile'
-
-	const timeAgo: string = timeSince(data?.created_at_i)
-	const TimeAgo = () => (
-		<Link className='hover:text-pink-400 hover:duration-200' to={commentsLink}>
-			{timeAgo}
-		</Link>
-	)
-
-	const author = data.author
-	const Author = () => (
-		<Link
-			className='hover:text-pink-400 hover:duration-200 text-sky-500 drop-shadow-sky-400-01 hover:drop-shadow-pink-400-02'
-			to={profileLink}
-		>
-			{author}
-		</Link>
-	)
-
 	return (
 		<div
 			className={classNames(
-				'grid grid-cols-[60px_auto] grid-rows-2',
+				'grid grid-cols-[60px_auto] auto-rows-auto',
 				className
 			)}
 		>
-			<button
-				className={classNames(
-					'row-span-full hover:text-pink-400 hover:duration-200 col-span-1 drop-shadow-md ',
-					vote
-						? 'text-pink-400 hover:drop-shadow-sky-400 hover:text-sky-400 font-medium'
-						: 'text-stone-400 hover:drop-shadow-pink-400 '
-				)}
-				onClick={toogleVote}
-			>
-				<div>▲</div>
-				<div>{data.points + (vote ? 1 : 0)}</div>
-			</button>
+			<PointsButton vote={vote} points={data.points} onClick={toogleVote} />
 
-			<div className='gap-x-1 flex col-span-2 row-start-1 row-end-1'>
-				<h3 className='text-slate-600 font-medium'>{data.title}</h3>
-
+			<div className='col-span-2 row-start-1 row-end-1'>
+				<h3 className='text-slate-600 inline font-medium'>{data.title}</h3>{' '}
 				{data.url && (
 					<a
-						className='text-stone-500 hover:text-pink-400 hover:duration-200 hover:drop-shadow-pink-400-02 font-light'
+						className='text-stone-500 hover:text-pink-400 hover:duration-200 hover:drop-shadow-pink-400-02 inline font-light'
 						href={data.url}
 						target='_blank'
 					>{`(${data.url})`}</a>
 				)}
 			</div>
-			<div className='text-stone-400 col-span-2 row-start-2 row-end-2'>
-				by <Author />
-				<TimeAgo />
-				<PostButtons
-					className='123'
+			<div className='col-span-2 row-start-2 row-end-2'>
+				<RecordInfo
+					type='default'
+					author={data.author}
+					dateTimeStamp={data.created_at_i}
 					onHideClick={() => {}}
 					commentsCount={data.num_comments}
-					commentsLink={commentsLink}
 				/>
 			</div>
 		</div>
