2aa36dee2a12ce0a3e13dcab8767c43dc3c12509
Ivan Baluta <ltlaitoff@gmail.com>
2022-05-02 16:06:33 +0300

    Created getInputValue helper in DatePicker

---
+++ src/components/DatePicker/helpers/getInputValue.ts
@@ -0,0 +1,56 @@
+export const getZeroPadded = (value: number | string): string => {
+	const stringifyValue = String(value)
+
+	return stringifyValue.length === 1 ? '0' + stringifyValue : stringifyValue
+}
+
+const getFormattedDateEntriesSort = (
+	prev: [string, string | number],
+	next: [string, string | number]
+): number => {
+	const equal = prev[0].length === next[0].length
+	const prevLessNext = prev[0].length < next[0].length
+
+	return equal ? 0 : prevLessNext ? 1 : -1
+}
+
+type getFormattedDateOptions = {
+	Y: number | string
+	M: number | string
+	MM: number | string
+	d: number | string
+	dd: number | string
+}
+
+export const getFormattedDate = (
+	format: string,
+	options: getFormattedDateOptions
+): string => {
+	const sortedOptions = Object.entries(options).sort(
+		getFormattedDateEntriesSort
+	)
+
+	return sortedOptions.reduce(
+		(prev, [key, value]) => prev.replaceAll(key, String(value)),
+		format
+	)
+}
+
+export const getInputValue = (
+	dateValue: Date,
+	format: string
+): string | null => {
+	if (dateValue.toString() === 'Invalid Date') {
+		return null
+	}
+
+	const formatterOptions: getFormattedDateOptions = {
+		Y: dateValue.getFullYear(),
+		M: dateValue.getMonth() + 1,
+		MM: getZeroPadded(dateValue.getMonth() + 1),
+		d: dateValue.getDate(),
+		dd: getZeroPadded(dateValue.getDate())
+	}
+
+	return getFormattedDate(format, formatterOptions)
+}
