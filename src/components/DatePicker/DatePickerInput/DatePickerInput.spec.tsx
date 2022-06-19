d89ee386c5c90918549eee9de7c49ec8fe7054d1
Ivan Baluta <ltlaitoff@gmail.com>
2022-06-19 16:10:00 +0300

    refactor: Refacting DatePickerInput tests

--- src/components/DatePicker/DatePickerInput/DatePickerInput.spec.tsx
+++ src/components/DatePicker/DatePickerInput/DatePickerInput.spec.tsx
@@ -3,30 +3,43 @@ import { screen, render } from '@testing-library/react'
 import user from '@testing-library/user-event'
 import '@testing-library/jest-dom'
 import DatePickerInput from './DatePickerInput'
+import { DatePickerInputProps } from './DatePickerInput.interfaces'
+
+const setup = ({
+	date = new Date('01-01-2022'),
+	format = 'dd-MM-Y',
+	onSubmit = jest.fn(),
+	onError = jest.fn(),
+	disabled = false,
+	...args
+}: Partial<DatePickerInputProps> = {}) => {
+	render(
+		<DatePickerInput
+			date={date}
+			format={format}
+			onSubmit={onSubmit}
+			onError={onError}
+			disabled={disabled}
+			{...args}
+		/>
+	)
+}
 
 describe('DatePickerInput', () => {
 	it('DatePickerInput should be in the document', () => {
-		render(
-			<DatePickerInput
-				date={new Date('01-01-2022')}
-				format={'dd-MM-Y'}
-				onSubmit={jest.fn()}
-				onError={jest.fn()}
-			/>
-		)
+		setup()
 
 		const input = screen.getByTestId('input')
-
 		expect(input).toBeInTheDocument()
 	})
 
 	describe.each`
-		date                      | format       | inputValue      | inputValueDate
-		${new Date('01-01-2022')} | ${'dd-MM-Y'} | ${'31-12-2022'} | ${new Date('12-31-2022')}
-		${new Date('01-02-2022')} | ${'d-M-Y'}   | ${'3-2-2022'}   | ${new Date('02-03-2022')}
+		format       | inputValue      | inputValueDate
+		${'dd-MM-Y'} | ${'31-12-2022'} | ${new Date('12-31-2022')}
+		${'d-M-Y'}   | ${'3-2-2022'}   | ${new Date('02-03-2022')}
 	`(
-		'date = $date, format = $format, valid data = $inputValue',
-		({ date, format, inputValue, inputValueDate }) => {
+		'onSubmit should be called with format = $format and valid inputValueDate = $inputValueDate',
+		({ format, inputValue, inputValueDate }) => {
 			let onSubmit = jest.fn()
 			let onError = jest.fn()
 			let input: HTMLInputElement
@@ -35,19 +48,12 @@ describe('DatePickerInput', () => {
 				onSubmit = jest.fn()
 				onError = jest.fn()
 
-				render(
-					<DatePickerInput
-						date={date}
-						format={format}
-						onSubmit={onSubmit}
-						onError={onError}
-					/>
-				)
+				setup({ format, onSubmit, onError })
 
 				input = screen.getByTestId('input') as HTMLInputElement
 			})
 
-			it('Valid data entry and press Enter button, onSubmit should be called', () => {
+			it('Valid data entry and press Enter button', () => {
 				user.clear(input)
 				user.type(input, inputValue + '{Enter}')
 
@@ -56,7 +62,7 @@ describe('DatePickerInput', () => {
 				expect(onError).toBeCalledWith(false)
 			})
 
-			it('Valid data entry and blur from input, onSubmit should be called', () => {
+			it('Valid data entry and blur from input', () => {
 				user.clear(input)
 				user.type(input, inputValue)
 				user.tab()
@@ -65,36 +71,16 @@ describe('DatePickerInput', () => {
 				expect(input).toHaveAttribute('data-error', 'false')
 				expect(onError).toBeCalledWith(false)
 			})
-
-			// TODO: Move it
-			it('Not valid data entry and press Enter button, onSubmit should be NOT called', () => {
-				user.clear(input)
-				user.type(input, 'not valid data{Enter}')
-
-				expect(onSubmit).not.toBeCalled()
-				expect(input).toHaveAttribute('data-error', 'true')
-				expect(onError).toBeCalledWith(true)
-			})
-
-			it('Not valid data entry and blur from input, onSubmit should be NOT called', () => {
-				user.clear(input)
-				user.type(input, 'not valid data')
-				user.tab()
-
-				expect(onSubmit).not.toBeCalled()
-				expect(input).toHaveAttribute('data-error', 'true')
-				expect(onError).toBeCalledWith(true)
-			})
 		}
 	)
 
 	describe.each`
-		stringDate      | format       | inputValue
-		${'01-01-2022'} | ${'dd-MM-Y'} | ${'31-12-2022'}
-		${'07-07-2022'} | ${'dd-MM-Y'} | ${'31-12-2022'}
+		inputValueDate
+		${'test'}
+		${'invalid'}
 	`(
-		'date = $stringDate, format = $format, valid data = $inputValue, disabled = true',
-		({ stringDate, format, inputValue }) => {
+		'onSubmit should be NOT called with invalid inputValueDate = $inputValueDate',
+		({ inputValueDate }) => {
 			let onSubmit = jest.fn()
 			let onError = jest.fn()
 			let input: HTMLInputElement
@@ -103,130 +89,144 @@ describe('DatePickerInput', () => {
 				onSubmit = jest.fn()
 				onError = jest.fn()
 
-				render(
-					<DatePickerInput
-						date={new Date(stringDate)}
-						format={format}
-						onSubmit={onSubmit}
-						onError={onError}
-						disabled={true}
-					/>
-				)
+				setup({ onSubmit, onError })
 
 				input = screen.getByTestId('input') as HTMLInputElement
 			})
 
-			it('Valid data entry and press Enter button, onSubmit should NOT be called and input value should be not changes', () => {
-				const inputValueBefore = input.value
-
+			it('Invalid data entry and press Enter button', () => {
 				user.clear(input)
-				user.type(input, inputValue + '{Enter}')
-
-				expect(inputValueBefore).toBe(input.value)
+				user.type(input, inputValueDate + '{Enter}')
 
 				expect(onSubmit).not.toBeCalled()
-				expect(input).toHaveAttribute('data-error', 'false')
-				expect(onError).not.toBeCalled()
+				expect(input).toHaveAttribute('data-error', 'true')
+				expect(onError).toBeCalledWith(true)
 			})
 
-			it('Valid data entry and blur from input, onSubmit should be NOT called and input value should be not changes', () => {
-				const inputValueBefore = input.value
-
+			it('Invalid data entry and blur from input', () => {
 				user.clear(input)
-				user.type(input, inputValue)
+				user.type(input, inputValueDate)
 				user.tab()
 
-				expect(inputValueBefore).toBe(input.value)
-
 				expect(onSubmit).not.toBeCalled()
-				expect(input).toHaveAttribute('data-error', 'false')
-				expect(onError).not.toBeCalled()
-			})
-
-			it('Not valid data entry and press Enter button, onSubmit should be NOT called and input value should be not changes', () => {
-				const inputValueBefore = input.value
-
-				user.clear(input)
-				user.type(input, 'not valid data{Enter}')
-
-				expect(inputValueBefore).toBe(input.value)
-
-				expect(onSubmit).not.toBeCalled()
-				expect(input).toHaveAttribute('data-error', 'false')
-				expect(onError).not.toBeCalled()
-			})
-
-			it('Not valid data entry and blur from input, onSubmit should be NOT called and input value should be not changes', () => {
-				const inputValueBefore = input.value
-
-				user.clear(input)
-				user.type(input, 'not valid data')
-				user.tab()
-
-				expect(inputValueBefore).toBe(input.value)
-
-				expect(onSubmit).not.toBeCalled()
-				expect(input).toHaveAttribute('data-error', 'false')
-				expect(onError).not.toBeCalled()
+				expect(input).toHaveAttribute('data-error', 'true')
+				expect(onError).toBeCalledWith(true)
 			})
 		}
 	)
+
+	describe('onSubmit should NOT be called and input value should be not changes with disabled = true, inputValue = "test"', () => {
+		const inputValue = 'test'
+		let onSubmit = jest.fn()
+		let onError = jest.fn()
+		let input: HTMLInputElement
+
+		beforeEach(() => {
+			onSubmit = jest.fn()
+			onError = jest.fn()
+
+			setup({
+				onError,
+				onSubmit,
+				disabled: true
+			})
+
+			input = screen.getByTestId('input') as HTMLInputElement
+		})
+
+		it('Valid data entry and press Enter button', () => {
+			const inputValueBefore = input.value
+
+			user.clear(input)
+			user.type(input, inputValue + '{Enter}')
+
+			expect(inputValueBefore).toBe(input.value)
+
+			expect(onSubmit).not.toBeCalled()
+			expect(input).toHaveAttribute('data-error', 'false')
+			expect(onError).not.toBeCalled()
+		})
+
+		it('Valid data entry and blur from input', () => {
+			const inputValueBefore = input.value
+
+			user.clear(input)
+			user.type(input, inputValue)
+			user.tab()
+
+			expect(inputValueBefore).toBe(input.value)
+
+			expect(onSubmit).not.toBeCalled()
+			expect(input).toHaveAttribute('data-error', 'false')
+			expect(onError).not.toBeCalled()
+		})
+
+		it('Not valid data entry and press Enter button', () => {
+			const inputValueBefore = input.value
+
+			user.clear(input)
+			user.type(input, 'not valid data{Enter}')
+
+			expect(inputValueBefore).toBe(input.value)
+
+			expect(onSubmit).not.toBeCalled()
+			expect(input).toHaveAttribute('data-error', 'false')
+			expect(onError).not.toBeCalled()
+		})
+
+		it('Not valid data entry and blur from input', () => {
+			const inputValueBefore = input.value
+
+			user.clear(input)
+			user.type(input, 'not valid data')
+			user.tab()
+
+			expect(inputValueBefore).toBe(input.value)
+
+			expect(onSubmit).not.toBeCalled()
+			expect(input).toHaveAttribute('data-error', 'false')
+			expect(onError).not.toBeCalled()
+		})
+	})
 
 	it.each`
 		format
 		${'test'}
 		${'random format'}
-	`('with not valid format = $format', ({ format }) => {
-		let onSubmit = jest.fn()
-		let onError = jest.fn()
+	`(
+		'with not valid format = $format onError should be called',
+		({ format }) => {
+			let onError = jest.fn()
 
-		render(
-			<DatePickerInput
-				date={new Date('01-01-2022')}
-				format={format}
-				onSubmit={onSubmit}
-				onError={onError}
-			/>
-		)
+			setup({ format, onError })
 
-		const input = screen.getByTestId('input')
+			const input = screen.getByTestId('input')
 
-		expect(onError).toBeCalledWith(true)
-		expect(input).toHaveAttribute('data-error', 'true')
-	})
+			expect(input).toHaveAttribute('data-error', 'true')
+			expect(onError).toBeCalledWith(true)
+		}
+	)
 
 	it.each`
 		date
 		${new Date('test')}
-	`('with not valid default date = $date', ({ date }) => {
-		let onSubmit = jest.fn()
-		let onError = jest.fn()
+	`(
+		'with not valid default date = $date onError should be called',
+		({ date }) => {
+			let onError = jest.fn()
 
-		render(
-			<DatePickerInput
-				date={date}
-				format={'dd-MM-Y'}
-				onSubmit={onSubmit}
-				onError={onError}
-			/>
-		)
+			setup({ date, onError })
 
-		expect(onError).toBeCalledWith(true)
-	})
+			const input = screen.getByTestId('input')
+
+			expect(input).toHaveAttribute('data-error', 'true')
+			expect(onError).toBeCalledWith(true)
+		}
+	)
 
 	it('test-attribute should be in html', () => {
-		let onSubmit = jest.fn()
-		let onError = jest.fn()
-
-		render(
-			<DatePickerInput
-				date={new Date('01-01-2022')}
-				format={'dd-MM-Y'}
-				onSubmit={onSubmit}
-				onError={onError}
-				test-attribute={'test'}
-			/>
-		)
+		// @ts-expect-error
+		setup({ 'test-attribute': 'test' })
 
 		const input = screen.getByTestId('input')
 
