449f9d986d2c68e793ad60f261b97e671fc9ae8a
Ivan Baluta <ltlaitoff@gmail.com>
2022-06-30 19:51:55 +0300

    feat: Added onKeyPressObserver mounting in App component

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,10 +1,19 @@
-import React, { FC, Suspense } from 'react'
+import React, { FC, Suspense, useEffect } from 'react'
 import Header from 'components/Header'
 
 import Router from 'routes'
 import Loader from 'components/Loader'
+import { onKeyPressObserver } from 'observers'
 
 const App: FC = () => {
+	useEffect(() => {
+		onKeyPressObserver.mount()
+
+		return () => {
+			onKeyPressObserver.unmount()
+		}
+	}, [])
+
 	return (
 		<div className='m-0-auto w-4/5 mx-auto my-0'>
 			<Suspense fallback={<Loader type='pink' />}>
