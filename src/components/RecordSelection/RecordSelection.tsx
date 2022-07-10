de39e9cf92c2389675030c0a2acb01aebf43aa37
Ivan Baluta <ltlaitoff@gmail.com>
2022-07-11 00:32:32 +0300

    feat(RecordSelection, redux): Added usage redux store for saving filters value

--- src/components/RecordSelection/RecordSelection.tsx
+++ src/components/RecordSelection/RecordSelection.tsx
@@ -1,14 +1,29 @@
 import React, { FC, useState } from 'react'
 import Filter from './components/Filter'
+import { CurrentFiltersItem } from './components/Filter/Filter.interfaces'
 import Search from './components/Search'
+import { filters } from 'data/filters'
+import { changeFilters, selectFilters } from 'store'
+import { useAppDispatch, useAppSelector } from 'hooks/reduxHooks'
 
 const RecordSelection: FC = () => {
 	const [searchValue, setSearchValue] = useState<string>('')
 
+	const currentFilters = useAppSelector(selectFilters)
+	const dispatch = useAppDispatch()
+
+	const setCurrentFilters = (array: CurrentFiltersItem[]) => {
+		dispatch(changeFilters(array))
+	}
+
 	return (
 		<div>
-			<Search onSubmit={setSearchValue} />
-			<Filter />
+			<Search defaultValue={searchValue} onSubmit={setSearchValue} />
+			<Filter
+				filters={filters}
+				currentFilters={currentFilters}
+				onChange={setCurrentFilters}
+			/>
 		</div>
 	)
 }
