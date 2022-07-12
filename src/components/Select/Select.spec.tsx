010a0e6dd5761ab376bb43f83e126866a58d4c10
Ivan Baluta <ltlaitoff@gmail.com>
2022-07-12 23:44:55 +0300

    test: Created tests for Select component

--- src/components/Select/Select.spec.tsx
+++ src/components/Select/Select.spec.tsx
@@ -1,28 +1,51 @@
 import React from 'react'
-import { screen, render } from '@testing-library/react'
+import { screen, render, within, act } from '@testing-library/react'
 import user from '@testing-library/user-event'
 import '@testing-library/jest-dom'
 import Select from './Select'
-import { SelectRecord } from './Select.interfaces'
+import { SelectProps, SelectRecord } from './Select.interfaces'
 import { ListProps } from 'components/List'
+import { SelectBaseProps } from './components'
+import { onKeyPressObserver } from 'observers'
 
-jest.doMock('components/List', () => {
-	const List = () => <div />
+onKeyPressObserver.mount()
+
+let mockListProps = {} as ListProps
+let mockSelectBaseProps = {} as SelectBaseProps
+
+jest.mock('components/List', () => {
+	const List = (props: ListProps) => {
+		mockListProps = { ...props }
+
+		return <div data-testid='list' />
+	}
 	return List
+})
+
+jest.mock('./components', () => {
+	const SelectBase = (props: SelectBaseProps) => {
+		mockSelectBaseProps = { ...props }
+
+		return <div data-testid='select-base' />
+	}
+
+	return {
+		SelectBase: SelectBase
+	}
 })
 
 const OPTIONS: SelectRecord[] = [
 	{
 		id: 0,
-		label: 'first'
+		label: 'zero'
 	},
 	{
 		id: 1,
-		label: 'second'
+		label: 'first'
 	},
 	{
 		id: 2,
-		label: 'third'
+		label: 'second'
 	}
 ]
 
@@ -30,8 +53,14 @@ const setup = ({
 	onClick = jest.fn(),
 	onChange = jest.fn(),
 	disabled = false,
-	defaultSelectId = 0
+	defaultSelectId = 0,
+	customAttributes = {}
+}: Partial<SelectProps> & {
+	customAttributes?: Record<string, string>
 } = {}) => {
+	mockListProps = {} as ListProps
+	mockSelectBaseProps = {} as SelectBaseProps
+
 	render(
 		<Select
 			options={OPTIONS}
@@ -39,132 +68,313 @@ const setup = ({
 			onChange={onChange}
 			disabled={disabled}
 			defaultSelectId={defaultSelectId}
+			{...customAttributes}
 		/>
 	)
 }
 
+beforeEach(() => {
+	expect.hasAssertions()
+})
+
 describe('Select', () => {
-	// it('Select should be in document', () => {
-	// 	setup()
-	// 	const select = screen.getByTestId('select')
-	// 	expect(select).toBeInTheDocument()
-	// })
-	// it.only('SelectList should includes items', () => {
-	// 	setup()
-	// 	const base = screen.getByTestId('base')
-	// 	user.click(base)
-	// 	expect(SelectListMocked).toBeInTheDocument()
-	// 	// const list = screen.getByTestId('list')
-	// 	// const items = screen.getAllByTestId('item')
-	// 	// const listChildren = Array.from(list?.childNodes)
-	// 	// expect(listChildren.length === items.length).toBeTruthy()
-	// 	// listChildren.forEach((item, index) => {
-	// 	// 	expect(item === items[index]).toBeTruthy()
-	// 	// })
-	// 	// OPTIONS.forEach((item, index) => {
-	// 	// 	expect(item.label === items[index].textContent).toBeTruthy()
-	// 	// })
-	// })
-	// it('On click on current selected element should be called onClick function', () => {
-	// 	if (!mainButton || !wrapper) throw Error
-	// 	user.click(mainButton)
-	// 	const items = wrapper.getAllByTestId('item')
-	// 	user.click(items[0])
-	// 	expect(onClick).toBeCalledWith(0)
-	// })
-	// it('On click on NOT current selected element should be called onClick and onChange functions', () => {
-	// 	if (!mainButton || !wrapper) throw Error
-	// 	user.click(mainButton)
-	// 	const items = wrapper.getAllByTestId('item')
-	// 	user.click(items[1])
-	// 	expect(onClick).toBeCalledWith(1)
-	// 	expect(onChange).toBeCalledWith(1)
-	// })
-	// it('On click main-button Select should open a list of items', () => {
-	// 	if (!mainButton) throw Error
-	// 	user.click(mainButton)
-	// 	const list = wrapper?.getByTestId('list')
-	// 	expect(list).toBeInTheDocument()
-	// })
-	// it('On click outside component with opened list should closing the list', () => {
-	// 	const OPTIONS = [
-	// 		{
-	// 			id: 0,
-	// 			label: 'first'
-	// 		},
-	// 		{
-	// 			id: 1,
-	// 			label: 'second'
-	// 		},
-	// 		{
-	// 			id: 2,
-	// 			label: 'third'
-	// 		}
-	// 	]
-	// 	wrapper = render(
-	// 		<div data-testid='div'>
-	// 			<Select options={OPTIONS} onClick={onClick} onChange={onChange} />
-	// 		</div>
-	// 	)
-	// 	const mainButton = wrapper.getByTestId('main-button')
-	// 	user.click(mainButton)
-	// 	const list = wrapper.getByTestId('list')
-	// 	expect(list).toBeInTheDocument()
-	// 	const div = wrapper.getByTestId('div')
-	// 	user.click(div)
-	// 	expect(list).not.toBeInTheDocument()
-	// })
-	// it('On click main-button Select and Selec is disable should NOT open a list of items', () => {
-	// 	const OPTIONS = [
-	// 		{
-	// 			id: 0,
-	// 			label: 'first'
-	// 		},
-	// 		{
-	// 			id: 1,
-	// 			label: 'second'
-	// 		},
-	// 		{
-	// 			id: 2,
-	// 			label: 'third'
-	// 		}
-	// 	]
-	// 	wrapper = render(
-	// 		<Select
-	// 			options={OPTIONS}
-	// 			onClick={onClick}
-	// 			onChange={onChange}
-	// 			disabled
-	// 		/>
-	// 	)
-	// 	const mainButton = wrapper.getByTestId('main-button')
-	// 	user.click(mainButton)
-	// 	expect(() => wrapper?.getByTestId('list')).toThrow()
-	// })
-	// it('defaultSelectId element should be rendered in mainButton by default', () => {
-	// 	const OPTIONS = [
-	// 		{
-	// 			id: 0,
-	// 			label: 'first'
-	// 		},
-	// 		{
-	// 			id: 1,
-	// 			label: 'second'
-	// 		},
-	// 		{
-	// 			id: 2,
-	// 			label: 'third'
-	// 		}
-	// 	]
-	// 	wrapper = render(
-	// 		<Select
-	// 			options={OPTIONS}
-	// 			onClick={onClick}
-	// 			onChange={onChange}
-	// 			defaultSelectId={1}
-	// 		/>
-	// 	)
-	// 	const mainButton = wrapper.getByTestId('main-button')
-	// 	expect(mainButton.textContent?.includes(OPTIONS[1].label)).toBeTruthy()
-	// })
+	it('"select" should be in the document', () => {
+		setup()
+
+		const select = screen.getByTestId('select')
+		expect(select).toBeInTheDocument()
+	})
+
+	it('"select" should include "select-base"', () => {
+		setup()
+
+		const select = screen.getByTestId('select')
+		const base = within(select).getByTestId('select-base')
+
+		expect(base).toBeInTheDocument()
+	})
+
+	it('"select-base" should have props = {onClick: ==Function==, listIsOpened: false, disabled: false, selectedItem={id: 0, label: "zero"}}', () => {
+		setup()
+
+		expect(mockSelectBaseProps).toMatchObject({
+			disabled: false,
+			listIsOpened: false,
+			selectedItem: { id: 0, label: 'zero' }
+		})
+		expect(mockSelectBaseProps.onClick).toBeInstanceOf(Function)
+	})
+
+	describe('After call base onClick', () => {
+		it('"select-base" should re render with props = {onClick: ==Function==, listIsOpened: true, disabled: false, selectedItem={id: 0, label: "zero"}}', () => {
+			setup()
+
+			act(() => {
+				mockSelectBaseProps.onClick()
+			})
+
+			expect(mockSelectBaseProps).toMatchObject({
+				disabled: false,
+				listIsOpened: true,
+				selectedItem: { id: 0, label: 'zero' }
+			})
+
+			expect(mockSelectBaseProps.onClick).toBeInstanceOf(Function)
+		})
+
+		it('"list" should be in the "select"', () => {
+			setup()
+
+			act(() => {
+				mockSelectBaseProps.onClick()
+			})
+
+			const select = screen.getByTestId('select')
+			const list = within(select).getByTestId('list')
+
+			expect(list).toBeInTheDocument()
+		})
+
+		it('"list" should have props = {options: [{ id: 0, label: "zero" },{ id: 1, label: "first" },{ id: 2, label: "second" }], onItemClick: ==Function==, selectedItem: {id: 0, label: "zero"}, onOutsideClick: ==Function==}', () => {
+			setup()
+
+			act(() => {
+				mockSelectBaseProps.onClick()
+			})
+
+			expect(mockListProps).toMatchObject({
+				selectedItem: { id: 0, label: 'zero' },
+				options: [
+					{ id: 0, label: 'zero' },
+					{ id: 1, label: 'first' },
+					{ id: 2, label: 'second' }
+				]
+			})
+
+			expect(mockListProps.onItemClick).toBeInstanceOf(Function)
+			expect(mockListProps.onOutsideClick).toBeInstanceOf(Function)
+		})
+
+		describe('After call onItemClick from "list" with arg = currentSelectedItem = {id: 0, label: "zero"}', () => {
+			it('"list" should NOT be in the document', () => {
+				setup()
+
+				act(() => {
+					mockSelectBaseProps.onClick()
+				})
+
+				act(() => {
+					mockListProps.onItemClick({ id: 0, label: 'zero' })
+				})
+
+				const list = screen.queryByTestId('list')
+				expect(list).not.toBeInTheDocument()
+			})
+
+			it('"select-base" should re render with props = {onClick: ==Function==, listIsOpened: false, disabled: false, selectedItem: {id: 0, label: "zero"}}', () => {
+				setup()
+
+				act(() => {
+					mockSelectBaseProps.onClick()
+				})
+
+				act(() => {
+					mockListProps.onItemClick({ id: 0, label: 'zero' })
+				})
+
+				expect(mockSelectBaseProps).toMatchObject({
+					disabled: false,
+					listIsOpened: false,
+					selectedItem: { id: 0, label: 'zero' }
+				})
+
+				expect(mockSelectBaseProps.onClick).toBeInstanceOf(Function)
+			})
+
+			it('onClick should be called with id = 0', () => {
+				const onClick = jest.fn()
+				setup({ onClick })
+
+				act(() => {
+					mockSelectBaseProps.onClick()
+				})
+
+				act(() => {
+					mockListProps.onItemClick({ id: 0, label: 'zero' })
+				})
+
+				expect(onClick).toBeCalledWith(0)
+			})
+		})
+
+		describe('After call onItemClick from "list" with arg = {id: 1, label: "first"} !== currentSelectedItem', () => {
+			it('"list" should NOT be in the document', () => {
+				setup()
+
+				act(() => {
+					mockSelectBaseProps.onClick()
+				})
+
+				act(() => {
+					mockListProps.onItemClick({ id: 1, label: 'first' })
+				})
+
+				const list = screen.queryByTestId('list')
+				expect(list).not.toBeInTheDocument()
+			})
+
+			it('onClick should be called with id = 1', () => {
+				const onClick = jest.fn()
+				setup({ onClick })
+
+				act(() => {
+					mockSelectBaseProps.onClick()
+				})
+
+				act(() => {
+					mockListProps.onItemClick({ id: 1, label: 'first' })
+				})
+
+				expect(onClick).toBeCalledWith(1)
+			})
+
+			it('onChange should be called with id = 1', () => {
+				const onChange = jest.fn()
+				setup({ onChange })
+
+				act(() => {
+					mockSelectBaseProps.onClick()
+				})
+
+				act(() => {
+					mockListProps.onItemClick({ id: 1, label: 'first' })
+				})
+
+				expect(onChange).toBeCalledWith(1)
+			})
+
+			it('"selest-base" should re render with props = {onClick: ==Function==, listIsOpened: false, disabled: false, selectedItem: {id: 1, label: "first"}}', () => {
+				setup()
+
+				act(() => {
+					mockSelectBaseProps.onClick()
+				})
+
+				act(() => {
+					mockListProps.onItemClick({ id: 1, label: 'first' })
+				})
+
+				expect(mockSelectBaseProps).toMatchObject({
+					disabled: false,
+					listIsOpened: false,
+					selectedItem: { id: 1, label: 'first' }
+				})
+
+				expect(mockSelectBaseProps.onClick).toBeInstanceOf(Function)
+			})
+		})
+
+		describe('After call onOutsideClick from "list"', () => {
+			it('"list" should NOT be in the document', () => {
+				setup()
+
+				act(() => {
+					mockSelectBaseProps.onClick()
+				})
+
+				act(() => {
+					mockListProps.onOutsideClick()
+				})
+
+				const list = screen.queryByTestId('list')
+				expect(list).not.toBeInTheDocument()
+			})
+
+			it('"select-base" should re render with props = {onClick: ==Function==, listIsOpened: false, disabled: false, selectedItem={id: 0, label: "zero"}}', () => {
+				setup()
+
+				act(() => {
+					mockSelectBaseProps.onClick()
+				})
+
+				act(() => {
+					mockListProps.onOutsideClick()
+				})
+
+				expect(mockSelectBaseProps).toMatchObject({
+					disabled: false,
+					listIsOpened: false,
+					selectedItem: { id: 0, label: 'zero' }
+				})
+
+				expect(mockSelectBaseProps.onClick).toBeInstanceOf(Function)
+			})
+		})
+		describe('After press Esc button', () => {
+			it('"list" should NOT be in the document', () => {
+				setup()
+
+				act(() => {
+					mockSelectBaseProps.onClick()
+				})
+
+				user.keyboard('{Escape}')
+
+				const list = screen.queryByTestId('list')
+				expect(list).not.toBeInTheDocument()
+			})
+
+			it('"select-base" should re render with props = {onClick: ==Function==, listIsOpened: false, disabled: false, selectedItem={id: 0, label: "zero"}}', () => {
+				setup()
+
+				act(() => {
+					mockSelectBaseProps.onClick()
+				})
+
+				user.keyboard('{Escape}')
+
+				expect(mockSelectBaseProps).toMatchObject({
+					disabled: false,
+					listIsOpened: false,
+					selectedItem: { id: 0, label: 'zero' }
+				})
+
+				expect(mockSelectBaseProps.onClick).toBeInstanceOf(Function)
+			})
+		})
+	})
+
+	describe('With disabled = true', () => {
+		describe('After call "select-base" onClick', () => {
+			it('"list" should NOT be in the document', () => {
+				setup({ disabled: true })
+
+				mockSelectBaseProps.onClick()
+
+				const list = screen.queryByTestId('list')
+				expect(list).not.toBeInTheDocument()
+			})
+		})
+	})
+
+	describe('With defaultSelectId = 1', () => {
+		it('"select-base" should rendered with props = {listIsOpened: false, disabled: false, selectedItem: {id: 1, label: "first"}, onClick: ==Function==}', () => {
+			setup({ defaultSelectId: 1 })
+
+			expect(mockSelectBaseProps).toMatchObject({
+				disabled: false,
+				listIsOpened: false,
+				selectedItem: { id: 1, label: 'first' }
+			})
+
+			expect(mockSelectBaseProps.onClick).toBeInstanceOf(Function)
+		})
+	})
+
+	it('Custom attribute data-hello="1" should be in "select"', () => {
+		setup({ customAttributes: { 'data-hello': '1' } })
+
+		const select = screen.getByTestId('select')
+		expect(select).toHaveAttribute('data-hello', '1')
+	})
 })
