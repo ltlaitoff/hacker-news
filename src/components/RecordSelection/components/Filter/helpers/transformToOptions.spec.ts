e72c3f9d01b7197188bcf24910d316f79acaee4c
Ivan Baluta <ltlaitoff@gmail.com>
2022-07-22 19:00:08 +0300

    feat: Created transformToOptions helpers in Filter component

---
+++ src/components/RecordSelection/components/Filter/helpers/transformToOptions.spec.ts
@@ -0,0 +1,11 @@
+import ColoredConsoleLogTemplates from 'utils/colors'
+
+ColoredConsoleLogTemplates.todo(
+	'Rewrite tests for Filter/transformToOptions (#77)'
+)
+
+describe('Filter/transformToOptions', () => {
+	it('-', () => {
+		expect(true).toBe(true)
+	})
+})
