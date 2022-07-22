0fdc471655cd342f0ed58e0b790329a7f4f31a80
Ivan Baluta <ltlaitoff@gmail.com>
2022-07-22 19:03:55 +0300

    feat: Created Filter/FilterValuePicker component

---
+++ src/components/RecordSelection/components/Filter/components/FilterValuePicker/FilterValuePicker.tsx
@@ -0,0 +1,80 @@
+import React, { FC } from 'react'
+import DatePicker from 'components/DatePicker'
+import NumberPicker from 'components/NumberPicker'
+import Select from 'components/Select'
+import { FilterBaseType } from 'typescript/filters'
+import { FILTER_DATE_FORMAT } from '../../constants'
+import { transformArrayToOptions } from '../../helpers'
+import { FilterValuePickerProps } from './FilterValuePicker.interfaces'
+
+const FilterValuePicker: FC<FilterValuePickerProps> = ({
+	filter,
+	onError,
+	onChange
+}) => {
+	switch (filter.type) {
+		case FilterBaseType.DATE:
+			return (
+				<div className='flex py-4'>
+					{filter.value instanceof Array ? (
+						<DatePicker
+							type='range'
+							value={filter.value}
+							onChange={onChange}
+							format={FILTER_DATE_FORMAT}
+							onError={onError}
+						/>
+					) : (
+						<DatePicker
+							type='standart'
+							value={filter.value}
+							onChange={onChange}
+							format={FILTER_DATE_FORMAT}
+							onError={onError}
+						/>
+					)}
+				</div>
+			)
+
+		case FilterBaseType.NUMBER:
+			return (
+				<div className='flex py-4'>
+					{filter.value instanceof Array ? (
+						<NumberPicker
+							type='range'
+							value={filter.value}
+							onChange={onChange}
+							aboveZero={true}
+							onError={onError}
+						/>
+					) : (
+						<NumberPicker
+							type='standart'
+							value={filter.value}
+							onChange={onChange}
+							aboveZero={true}
+							onError={onError}
+						/>
+					)}
+				</div>
+			)
+
+		case FilterBaseType.LIST: {
+			const onSelectItemClick = (filterId: number) => {
+				const value = filter.listValues[filterId]
+				onChange(value)
+			}
+
+			return (
+				<div className='flex py-4'>
+					<Select
+						options={transformArrayToOptions(filter.listValues)}
+						onClick={onSelectItemClick}
+					/>
+				</div>
+			)
+		}
+	}
+}
+
+export default FilterValuePicker
