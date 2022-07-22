893c31e3e5fa3c35e5b70008cf357a5753cbebdc
Ivan Baluta <ltlaitoff@gmail.com>
2022-07-22 18:58:26 +0300

    feat: Created formatFilterValueForOutput Filter helper

---
+++ src/components/RecordSelection/components/Filter/helpers/formatFilterValueForOutput.spec.ts
@@ -0,0 +1,11 @@
+import ColoredConsoleLogTemplates from 'utils/colors'
+
+ColoredConsoleLogTemplates.todo(
+	'Rewrite tests for Filter/formatFilterValueForOutput (#77)'
+)
+
+describe('Filter/formatFilterValueForOutput', () => {
+	it('-', () => {
+		expect(true).toBe(true)
+	})
+})
