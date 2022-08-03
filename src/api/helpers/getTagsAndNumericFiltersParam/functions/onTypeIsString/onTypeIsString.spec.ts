87b58e45118625f3a633439bda5933e70a961d0e
Ivan Baluta <ltlaitoff@gmail.com>
2022-08-03 19:59:34 +0300

    test: Write tests for onTypeIsString

--- src/api/helpers/getTagsAndNumericFiltersParam/functions/onTypeIsString/onTypeIsString.spec.ts
+++ src/api/helpers/getTagsAndNumericFiltersParam/functions/onTypeIsString/onTypeIsString.spec.ts
@@ -1,9 +1,49 @@
-import ColoredConsoleLogTemplates from 'utils/colors'
-
-ColoredConsoleLogTemplates.todo('Write tests on onTypeIsString(#85)')
+import { FilterBaseName, FilterBaseType } from 'typescript/filters'
+import { onTypeIsString } from '.'
 
 describe('api/helpers/getTagsAndNumericFiltersParam/functions/onTypeIsString', () => {
-	it('-', () => {
-		expect(true).toBe(true)
-	})
+	it.each`
+		value
+		${'test'}
+		${'random'}
+		${'reas'}
+		${''}
+	`(
+		'onTypeIsString with filter.value = $value and options = undefined should return $value',
+		({ value }) => {
+			const result = onTypeIsString({
+				id: 0,
+				name: FilterBaseName.AUTHOR,
+				type: FilterBaseType.STRING,
+				filtration: 'is',
+				value: value
+			})
+
+			expect(result).toEqual(value)
+		}
+	)
+
+	it.each`
+		value     | key       | result
+		${'test'} | ${'key'}  | ${'key_test'}
+		${'gg'}   | ${'user'} | ${'user_gg'}
+		${''}     | ${'key'}  | ${''}
+		${'test'} | ${''}     | ${'test'}
+	`(
+		'onTypeIsString with filter.value = $value and options = {key: $key} should return $result',
+		({ value, key, result }) => {
+			const functionResult = onTypeIsString(
+				{
+					id: 0,
+					name: FilterBaseName.AUTHOR,
+					type: FilterBaseType.STRING,
+					filtration: 'is',
+					value: value
+				},
+				{ key: key }
+			)
+
+			expect(functionResult).toEqual(result)
+		}
+	)
 })
--- src/api/helpers/getTagsAndNumericFiltersParam/functions/onTypeIsString/onTypeIsString.ts
+++ src/api/helpers/getTagsAndNumericFiltersParam/functions/onTypeIsString/onTypeIsString.ts
@@ -6,9 +6,13 @@ type onTypeIsStringOptions = {
 
 const onTypeIsString = (
 	filter: FilterStringReceived,
-	options: onTypeIsStringOptions
+	options: onTypeIsStringOptions = {}
 ): string => {
+	if (filter.value === '') return filter.value
+
 	if (options.key) {
+		if (options.key === '') return filter.value
+
 		return `${options.key}_${filter.value}`
 	}
 
