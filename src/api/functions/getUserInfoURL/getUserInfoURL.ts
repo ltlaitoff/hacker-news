75a68bd9dbd9b81ad858562a34285502db67a16d
Ivan Baluta <ltlaitoff@gmail.com>
2022-08-03 22:21:53 +0300

    test: Write tests for getUserInfoURL

--- src/api/functions/getUserInfoURL/getUserInfoURL.ts
+++ src/api/functions/getUserInfoURL/getUserInfoURL.ts
@@ -1,4 +1,4 @@
-import { USER_URL_TEMPLATE } from 'api/constants'
+import { USER_URL_TEMPLATE } from '../../constants'
 
 export const getUserInfoURL = (username: string) => {
 	return USER_URL_TEMPLATE + username
