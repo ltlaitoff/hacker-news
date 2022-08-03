fd0b0af8b22ae2686bd055a64b521225353f4df5
Ivan Baluta <ltlaitoff@gmail.com>
2022-08-03 19:14:50 +0300

    feat: Added handling of edge cases into api/helpers/getPageParam

--- src/api/helpers/getPageParam/getPageParam.spec.ts
+++ src/api/helpers/getPageParam/getPageParam.spec.ts
@@ -1,5 +1,11 @@
 import { getPageParam } from '.'
 
+const PAGE_DEFAULT_VALUE = 10
+
+jest.mock('../../constants', () => ({
+	PAGE_DEFAULT_VALUE
+}))
+
 describe('api/helpers/getPageParam', () => {
 	it.each`
 		pageNumber
@@ -12,6 +18,20 @@ describe('api/helpers/getPageParam', () => {
 		'getPageParam with page = $pageNumber should return "$pageNumber"',
 		({ pageNumber }) => {
 			expect(getPageParam(pageNumber)).toBe(String(pageNumber))
+		}
+	)
+
+	it.each`
+		pageNumber
+		${-1}
+		${'test'}
+		${NaN}
+		${Infinity}
+		${'jasd'}
+	`(
+		`getPageParam with not valid page = $pageNumber should return default value = ${PAGE_DEFAULT_VALUE}`,
+		({ pageNumber }) => {
+			expect(getPageParam(pageNumber)).toBe(String(PAGE_DEFAULT_VALUE))
 		}
 	)
 })
