525b7f7c63932f03fb68342091117e04e95d5b95
Ivan Baluta <ltlaitoff@gmail.com>
2022-09-18 11:14:33 +0300

    feat: Created getOrderedNumbers helpers function

--- src/helpers/get.ts
+++ src/helpers/get.ts
@@ -2,3 +2,14 @@ export function getFilledArray<T>(length: number, value: T): T[] {
 	if (length < 0) return []
 	return Array(length).fill(value)
 }
+
+export function getOrderedNumbers(
+	first: number,
+	second: number
+): [number, number] {
+	if (first < second) {
+		return [first, second]
+	}
+
+	return [second, first]
+}
