8eb96ac2787422eb126f594fd1e623736ec29d71
Ivan Baluta <ltlaitoff@gmail.com>
2022-09-18 11:19:25 +0300

    feat(NumberPicker types): Created Range types

--- src/components/NumberPicker/NumberPicker.interfaces.tsx
+++ src/components/NumberPicker/NumberPicker.interfaces.tsx
@@ -1,7 +1,3 @@
-export type NumberPickerStandartValue = number | [number, number]
-export type NumberPickerStandartValueWithNull = number | [number, number] | null
-export type NumberPickerStandartOnChange = number | [number, number]
-
 export enum onChangeType {
 	BLUR = 'blur',
 	ENTER_KEY = 'enterKey',
@@ -12,9 +8,14 @@ export enum stepType {
 	UP = 'up',
 	DOWN = 'down'
 }
-export interface NumberPickerProps {
-	value: number
-	onChange: (value: number, type: onChangeType) => void
+
+export type NumberPickerBaseType = number
+export type NumberPickerBaseRangeType = [
+	NumberPickerBaseType,
+	NumberPickerBaseType
+]
+
+interface NumberPickerBase {
 	error: boolean
 	onError?: (value: boolean) => void
 	disabled?: boolean
@@ -23,50 +24,12 @@ export interface NumberPickerProps {
 	min?: number
 }
 
-// import { NumberPickerInputProps } from './components/NumberPickerInput'
+export interface NumberPickerProps extends NumberPickerBase {
+	value: NumberPickerBaseType
+	onChange: (value: NumberPickerBaseType, type: onChangeType) => void
+}
 
-// export type DataPickerFormats = string
-
-// export type NumberInputProps = Omit<
-// 	Omit<NumberPickerInputProps, 'onSubmit'>,
-// 	'value'
-// > & {
-// 	value: [number, number]
-// 	onSubmit: (number: [number, number]) => void
-// 	onError?: (value: boolean) => void
-// 	aboveZero?: boolean
-// }
-
-// export type NumberPickerStandartValue = number | [number, number]
-// export type NumberPickerStandartValueWithNull =
-// 	| number
-// 	| [number, number]
-// 	| null
-// export type NumberPickerStandartOnChange = number | [number, number]
-
-// export type NumberPickerRangeValue = [number, number]
-// export type NumberPickerRangeValueWithNull = [number, number] | null
-// export type NumberPickerRangeOnChange = [number, number]
-
-// interface NumberPickerBase {
-// 	disabled?: boolean
-// 	className?: string
-// 	onError: (value: boolean) => void
-// 	aboveZero?: boolean
-// }
-
-// export interface NumberPickerStandartProps extends NumberPickerBase {
-// 	type?: 'standart'
-// 	value: number | [number, number] | null
-// 	onChange: (value: number | [number, number]) => void
-// }
-
-// export interface NumberPickerRangeProps extends NumberPickerBase {
-// 	type: 'range'
-// 	value: [number, number] | null
-// 	onChange: (value: [number, number]) => void
-// }
-
-// export type NumberPickerProps =
-// 	| NumberPickerStandartProps
-// 	| NumberPickerRangeProps
+export interface NumberPickerRangeProps extends NumberPickerBase {
+	value: NumberPickerBaseRangeType
+	onChange: (value: NumberPickerBaseRangeType, type: onChangeType) => void
+}
