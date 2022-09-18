16adf582e2574e39cc7fd1c2ba22428343dd9d0d
Ivan Baluta <ltlaitoff@gmail.com>
2022-09-18 22:28:00 +0300

    test: Writed tests for DatePicker/helpers/getDefaultDateValue

--- src/components/DatePicker/helpers/getDefaultDateValue.spec.ts
+++ src/components/DatePicker/helpers/getDefaultDateValue.spec.ts
@@ -1,11 +1,20 @@
-import ColoredConsoleLogTemplates from 'utils/colors'
+import { getDefaultDateValue } from './getDefaultDateValue'
 
-ColoredConsoleLogTemplates.todo(
-	'Write tests for DatePicker/getDefaultDateValue helper'
-)
+const DATE_NOW = new Date('01-01-2021')
+jest.useFakeTimers().setSystemTime(DATE_NOW)
 
 describe('DatePicker/getDefaultDateValue', () => {
-	it('-', () => {
-		expect(true).toBe(true)
-	})
+	it.each`
+		value                                               | result
+		${null}                                             | ${[DATE_NOW, DATE_NOW]}
+		${[null, new Date('01-01-2022')]}                   | ${[DATE_NOW, DATE_NOW]}
+		${[new Date('01-01-2022'), null]}                   | ${[DATE_NOW, DATE_NOW]}
+		${[new Date('01-01-2022'), new Date('01-01-2022')]} | ${[new Date('01-01-2022'), new Date('01-01-2022')]}
+		${new Date('01-01-2022')}                           | ${[new Date('01-01-2022'), new Date('01-01-2022')]}
+	`(
+		'getDefaultDateValue with value = $value should return $result',
+		({ value, result }) => {
+			expect(getDefaultDateValue(value)).toEqual(result)
+		}
+	)
 })
