05e540977de872af9d843072f5db2c9daf34a028
Ivan Schedrovskiy <ltlaitoff@gmail.com>
2023-10-12 18:58:01 +0300

    Change api url to https

--- src/api/constants/apiConstants.ts
+++ src/api/constants/apiConstants.ts
@@ -1,4 +1,4 @@
-const BASE_URL = 'http://hn.algolia.com/api/v1/'
+const BASE_URL = 'https://hn.algolia.com/api/v1/'
 
 export const ITEM_URL_TEMPLATE: string = BASE_URL + 'items/'
 export const USER_URL_TEMPLATE: string = BASE_URL + 'users/'
